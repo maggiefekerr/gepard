27ecb839e749a75d0cfb77a110f9f78278e19e3c
Maggie Kerr <maggiekerr@Maggies-MacBook-Pro.local>
2025-07-02 09:32:48 -0400

    added new branch

--- src/gepard/cff.py
+++ src/gepard/cff.py
@@ -428,12 +428,14 @@ class HybridCFF(MellinBarnesCFF, DispersionFixedPoleCFF, CFF):
 
     def ImH(self, pt, xi=0):
         """Imaginary part of CFF H."""
-        return MellinBarnesCFF.ImH(self, pt) + DispersionFixedPoleCFF.ImH(self, pt, xi)
+        #return MellinBarnesCFF.ImH(self, pt) + DispersionFixedPoleCFF.ImH(self, pt, xi)
+	return 0
 
     def ReH(self, pt):
         """Real part of CFF H."""
-        return MellinBarnesCFF.ReH(self, pt) + DispersionFixedPoleCFF.ReH(
-                self, pt, imfun=DispersionFixedPoleCFF.ImH)
+        #return MellinBarnesCFF.ReH(self, pt) + DispersionFixedPoleCFF.ReH(
+        #        self, pt, imfun=DispersionFixedPoleCFF.ImH)
+	return 0
 
     def ImE(self, pt, xi=0):
         """Imaginary part of CFF E."""
@@ -448,15 +450,18 @@ class HybridCFF(MellinBarnesCFF, DispersionFixedPoleCFF, CFF):
 
     def ImHt(self, pt, xi=0):
         """Imaginary part of CFF Ht."""
-        return DispersionFixedPoleCFF.ImHt(self, pt, xi)
+        #return DispersionFixedPoleCFF.ImHt(self, pt, xi)
+	return 0
 
     def ReHt(self, pt):
         """Real part of CFF Ht."""
-        return DispersionFixedPoleCFF.ReHt(self, pt, imfun=DispersionFixedPoleCFF.ImHt)
+        #return DispersionFixedPoleCFF.ReHt(self, pt, imfun=DispersionFixedPoleCFF.ImHt)
+	return 0
 
     def ImEt(self, pt):
         """Imaginary part of CFF Et."""
-        return DispersionFixedPoleCFF.ImEt(self, pt)
+        #return DispersionFixedPoleCFF.ImEt(self, pt)
+	return 0
 
     def ReEt(self, pt):
         """Real part of CFF Et. Provided by subclasses."""
